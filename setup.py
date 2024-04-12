306f51ed1fb10e5210aad41f89ae690f7f027277
stan-hua <stanley.z.hua@gmail.com>
2024-04-12 11:26:48 -0400

    Add setup and requirements file

---
+++ setup.py
@@ -0,0 +1,15 @@
+"""
+setup.py
+
+Description: Used to set up PIP package
+"""
+
+# Standard libraries
+from setuptools import setup, find_packages
+
+# Set up package
+setup(
+    name="zero_init",
+    version="0.1",
+    packages=find_packages(),
+)
