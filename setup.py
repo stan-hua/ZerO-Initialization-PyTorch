007535b64c326ba3535a86f5db0d6b29848a09a8
stan-hua <stanley.z.hua@gmail.com>
2024-04-12 11:43:26 -0400

    Update setup file

--- setup.py
+++ setup.py
@@ -10,6 +10,14 @@ from setuptools import setup, find_packages
 # Set up package
 setup(
     name="zero_init",
-    version="0.1",
+    version="0.2",
+    url="https://github.com/stan-hua/ZerO-Initialization-PyTorch",
+    author="Stanley Hua",
+    description="PyTorch implementation of ZerO Initialization",
     packages=find_packages(),
+    install_requires=["scipy>=0.8.0", "torch"],
+    keywords="pytorch implementation",
+    project_urls={
+        "Source": "https://github.com/stan-hua/ZerO-Initialization-PyTorch",
+    }
 )
